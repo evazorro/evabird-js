eaa3977e47528dd40584ef12eb55bacc8d0c8e15
Eva Parish <eva@replicate.com>
2024-10-03 10:37:02 -0400

    Very basic layout and styling done

---
+++ app/ui/search-form.tsx
@@ -0,0 +1,34 @@
+import { Button } from './button';
+
+export default function SearchForm() {
+    return (
+        <form className="space-y-3">
+            <div className="flex-1 rounded-lg px-6 pb-4">
+                <h1 className="mb-3 text-2xl">Enter your region to see recent notable bird sightings nearby:</h1>
+            </div>
+            <div>
+                <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="region"
+                >
+                Region
+                </label>
+                <div className="relative">
+                <input
+                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                    id="region"
+                    type="string"
+                    name="region"
+                    placeholder="Enter a region code (e.g., US-NY-047)"
+                    required
+                /> 
+                </div>
+            </div>
+            <Button className="mt-4 w-full">
+                Search
+            </Button>
+        </form>
+    );
+}
+
+
